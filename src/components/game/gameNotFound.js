40dabd173ab472a767fbc9cbd31fefb129a1ea29
Jordan Carroll <jordan.carroll@gtri.gatech.edu>
2017-07-31 18:19:16 -0400

    added Link to start a new game in the game not found component

--- src/components/game/gameNotFound.js
+++ src/components/game/gameNotFound.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 export default () => (
-    <h1>This game was not found.</h1>
+    <main>
+        <h1>This game was not found.</h1>
+        <Link to='/'>Start New Game</Link>
+    </main>
 )
